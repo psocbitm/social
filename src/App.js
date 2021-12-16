3142b2884ab3094045e8e0ca98519a8c7769af7a
x-ash-x <palashasati1@gmail.com>
2021-12-16 10:52:33 +0530

    create a login page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Home from "./pages/home/Home";
 import PersonIcon from "@mui/icons-material/Person";
 import Topbar from "./components/topbar/Topbar";
 import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
 function App() {
 	return (
 		<div>
 			{/* <Home/> */}
-			<Profile/>
+			{/* <Profile/> */}
+			<Login/>
 		</div>
 	);
 }
